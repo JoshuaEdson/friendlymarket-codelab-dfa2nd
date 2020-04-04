edaab32c8f2203333430060fb529a21619d6b498
JoshuaEdson <joshuafaburada_edson@yahoo.com.my>
2020-04-04 15:57:21 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# friendlymarket-codelab-dfa2nd
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/friendlymarket-codelab-dfa2nd)
---
+++ src/firebase-config.js
@@ -0,0 +1,15 @@
+/* 
+  Your Firebase config from the Firebase console 
+  https://firebase.google.com/docs/web/setup#config-object
+*/
+const firebaseConfig = {
+   apiKey: "AIzaSyAO2yK8DXBdKQ1vn2kBGRvTdm4JRTRBHgY",
+    authDomain: "friendlymarket-ed2a1.firebaseapp.com",
+    databaseURL: "https://friendlymarket-ed2a1.firebaseio.com",
+    projectId: "friendlymarket-ed2a1",
+    storageBucket: "friendlymarket-ed2a1.appspot.com",
+    messagingSenderId: "528480512763",
+    appId: "1:528480512763:web:4d4c092f0d08710dff1e5e"
+};
+
+export default firebaseConfig;
